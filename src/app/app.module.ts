571a8c0994ac54d682a8f69021f9aee387c89cc2
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-07-08 21:15:38 +0800

    [MOD] Add to route admin module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { QrCodeModule } from 'ng-qrcode';
+import {MatButtonModule} from '@angular/material/button'
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminModule } from './admin/admin.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AdminModule,
+    LoginModule,
     BrowserModule,
     AppRoutingModule,
     QrCodeModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+
     
   ],
   providers: [],
