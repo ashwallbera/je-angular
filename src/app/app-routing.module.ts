7e29c3158d89bfc0c293c91df6ac06bf2cae6fc0
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-07-08 21:16:36 +0800

    [MOD] ADD MODULE TO ROUTING

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    loadChildren: () => 
+    import('./admin/admin-routing.module').then((module) => module.AdminRoutingModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
