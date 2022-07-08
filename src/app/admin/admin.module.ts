db004f1eea110d7d8fb08adeca475ad02053c1c3
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-07-08 21:17:14 +0800

    [MOD] Add sidenav and button module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainComponent } from './main/main.component';
+import { MatDrawer, MatDrawerContainer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidenavComponent,
+    MainComponent,
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatSidenavModule,
+    MatButtonModule
+    
   ]
 })
 export class AdminModule { }
