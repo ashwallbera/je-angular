1952bae5c9af4a66e06eb78e7e27b87deb6cfe61
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-07-08 21:35:33 +0800

    [MOD] ADD Firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'je-angular';
+  
 }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVdGM3HB8YouOEHypwvLkjM_GWuo8S5pk",
+  authDomain: "jadocenterprises.firebaseapp.com",
+  databaseURL: "https://jadocenterprises-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jadocenterprises",
+  storageBucket: "jadocenterprises.appspot.com",
+  messagingSenderId: "1062296600910",
+  appId: "1:1062296600910:web:57b3c9d98146755e2e9e1f",
+  measurementId: "G-NZ5PRKD92S"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
